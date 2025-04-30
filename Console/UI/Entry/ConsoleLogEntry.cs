8693413462cfd6e8b3f0e9530c04405f4e203798
Marcel Marusiak <marcel.marusiak@gmail.com>
2025-04-30 14:42:21 +0200

    refactor: TypeLinker.cs for custom message types with custom colors

---
+++ Console/UI/Entry/ConsoleLogEntry.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Needle.Console.UI.Entry
+{
+    public class ConsoleLogEntry<T> where T : Enum
+    {
+        public T MessageType { get; }
+        public string Content { get; }
+        public string DevContent { get; }
+        public DateTime Timestamp { get; }
+        public object Source { get; }
+
+        public ConsoleLogEntry(T messageType, string content, DateTime timestamp, object source, string devContent)
+        {
+            MessageType = messageType;
+            Content = content;
+            Timestamp = timestamp;
+            Source = source;
+            DevContent = devContent;
+        }
+    }
+}
