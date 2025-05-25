7044170f17bd0cb3d32ca6c149037443490771d9
Marcel Marusiak <marcel.marusiak@gmail.com>
2025-05-25 17:44:00 +0200

    wip: suggestions - highlighting selected suggestion and displaying placeholder with it

--- Console/UI/UserInput/ConsoleInputField.cs
+++ Console/UI/UserInput/ConsoleInputField.cs
@@ -7,11 +7,16 @@ namespace NeedleAssets.Console.UI.UserInput
     public class ConsoleInputField : TMP_InputField 
     {
         private Suggestions.Suggestions _suggestions;
+        [SerializeField] private string originalPlaceholderLabel;
+        private TextMeshProUGUI _placeholder;
         
         private new void Awake()
         {
             base.Awake();
-            _suggestions = GetComponent<Suggestions.Suggestions>();    
+            _suggestions = GetComponent<Suggestions.Suggestions>();
+            _placeholder = placeholder.GetComponent<TextMeshProUGUI>();
+            if (_placeholder.text != string.Empty && originalPlaceholderLabel == string.Empty) originalPlaceholderLabel = _placeholder.text;
+            else _placeholder.text = originalPlaceholderLabel;
         }
         
         public override void OnUpdateSelected(UnityEngine.EventSystems.BaseEventData eventData) 
@@ -21,12 +26,14 @@ namespace NeedleAssets.Console.UI.UserInput
             { 
                 _suggestions.UpSelection();
                 placeholder.GetComponent<TextMeshProUGUI>().text = _suggestions.GetCurrentSuggestionSilently().Name;
+                placeholder.enabled = true;
                 return;
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 _suggestions.DownSelection();
                 placeholder.GetComponent<TextMeshProUGUI>().text = _suggestions.GetCurrentSuggestionSilently().Name;
+                placeholder.enabled = true;
                 return;
             }
 
@@ -35,9 +42,10 @@ namespace NeedleAssets.Console.UI.UserInput
                 var output = _suggestions.GetCurrentSuggestion();
                 text = output.Name;
                 caretPosition = text.Length;
+                _placeholder.text = originalPlaceholderLabel;
                 return;
             }
-            // add next suggestion
+            _placeholder.text = originalPlaceholderLabel;
             base.OnUpdateSelected(eventData); // call default behavior otherwise
         }
     }
