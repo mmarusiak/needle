cfbdb5b89afc5847ce130739aa78f3b92571d390
Marcel Marusiak <marcel.marusiak@gmail.com>
2025-05-28 20:12:36 +0200

    wip: last entries retrieval
    also note that I do not like how structure of UI (ConsoleInputField/Suggestions) look like. I would like to refactor them ASAP instead of constantly working on them while they look like messy shit.

--- Console/UI/UserInput/ConsoleInputField.cs
+++ Console/UI/UserInput/ConsoleInputField.cs
@@ -6,6 +6,11 @@ namespace NeedleAssets.Console.UI.UserInput
     [RequireComponent(typeof(Suggestions.Suggestions))]
     public class ConsoleInputField : TMP_InputField 
     {
+        private const int StackSize = 6;
+        private int _currentStackSize = 0;
+        private string[] _entriesStack = new string[StackSize];
+        private int _stackLookUp = -1;
+        
         private Suggestions.Suggestions _suggestions;
         [SerializeField] private string originalPlaceholderLabel;
         private TextMeshProUGUI _placeholder;
@@ -21,9 +26,18 @@ namespace NeedleAssets.Console.UI.UserInput
         
         public override void OnUpdateSelected(UnityEngine.EventSystems.BaseEventData eventData) 
         { 
-            // handle mobile/gamepads inputs!!!!!!
+            // to prevent unfocusing
             if (Input.GetKeyDown(KeyCode.UpArrow)) 
             { 
+                if (text == "")
+                {
+                    if (_currentStackSize == 0) return;
+                    _stackLookUp += 1;
+                    _stackLookUp %= _currentStackSize;
+                    _placeholder.text = _entriesStack[_currentStackSize - 1 - _stackLookUp];
+                    placeholder.enabled = true;
+                    return;
+                }
                 _suggestions.UpSelection();
                 _placeholder.text = _suggestions.GetCurrentSuggestionSilently().Name;
                 placeholder.enabled = true;
@@ -31,7 +45,20 @@ namespace NeedleAssets.Console.UI.UserInput
             }
             if (Input.GetKeyDown(KeyCode.DownArrow))
             {
+                if (text == "")
+                {
+                    if (_currentStackSize == 0) return;
+                    if (_stackLookUp != -1) _stackLookUp -= 1;
+                    _placeholder.text = _stackLookUp >= 0 ? _entriesStack[_currentStackSize - 1 - _stackLookUp] : originalPlaceholderLabel;
+                    placeholder.enabled = _stackLookUp >= 0;
+                    // get last from stack
+                    // draw placeholder and if tabbed then get it
+                    // use peek
+                    // maybe get only five/six last commands
+                    return;
+                }
                 _suggestions.DownSelection();
+                if (_suggestions.GetCurrentSuggestionSilently() == null) return;
                 _placeholder.text = _suggestions.GetCurrentSuggestionSilently().Name;
                 placeholder.enabled = true;
                 return;
@@ -39,13 +66,18 @@ namespace NeedleAssets.Console.UI.UserInput
 
             if (Input.GetKeyDown(KeyCode.Tab))
             {
+                if (text == "" && _stackLookUp != -1 && _currentStackSize != 0)
+                {
+                    text = _entriesStack[_currentStackSize - 1 - _stackLookUp];
+                    caretPosition = text.Length;
+                    return;
+                }
                 var output = _suggestions.GetCurrentSuggestion();
                 text = output.Name;
                 caretPosition = text.Length;
                 _placeholder.text = originalPlaceholderLabel;
                 return;
             }
-            // to prevent unfocusing
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 onEndEdit.Invoke(text);
@@ -57,5 +89,12 @@ namespace NeedleAssets.Console.UI.UserInput
         public void OnEdited() => _placeholder.text = originalPlaceholderLabel;
         
         public void Clear() => text = "";
+
+        public void AddEntryToStack(string entry)
+        {
+            if (_entriesStack.Length == StackSize) _entriesStack = _entriesStack[1..];
+            if (_currentStackSize < StackSize) _currentStackSize++;
+            _entriesStack[_currentStackSize - 1] = entry;
+        }
     }
 }
